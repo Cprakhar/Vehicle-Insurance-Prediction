8c6c2b2555fef863184f0fd4f0482639342361ff
Prakhar Chhalotre <cprakhar21@iitk.ac.in>
2025-04-19 21:55:13 +0530

    Initialize project structure with essential files: .gitignore, pyproject.toml, requirements.txt, setup.py, and egg-info metadata.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='1.0.0',
+    author='pchhalotre',
+    author_email='chhalotreprakhar00@gmail.com',
+    packages=find_packages()
+)
