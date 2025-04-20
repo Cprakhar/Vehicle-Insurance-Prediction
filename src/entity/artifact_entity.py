1c2f5ca1107b04769bf6727f7d68f1c20fb50198
Prakhar Chhalotre <cprakhar21@iitk.ac.in>
2025-04-21 02:10:22 +0530

    Implement data transformation, model training, evaluation, and pushing components

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionArtifact:
-
     '''This class is used to create the data ingestion artifact.'''
 
     training_filepath: str
@@ -10,9 +9,49 @@ class DataIngestionArtifact:
 
 @dataclass
 class DataValidationArtifact:
-    
     '''This class is used to create the data validation artifact.'''
 
-    validation_status:bool
+    validation_status: bool
     message: str
     validation_report_filepath: str
+
+
+@dataclass
+class DataTransformationArtifact:
+    '''This class is used to create the data transformation artifact.'''
+
+    transformed_object_filepath: str 
+    transformed_train_filepath: str
+    transformed_test_filepath: str
+
+@dataclass
+class ClassificationMetricArtifact:
+    '''This class is used to create the classification metric artifact.'''
+
+    accuracy_score:float
+    f1_score:float
+    precision_score:float
+    recall_score:float
+
+@dataclass
+class ModelTrainerArtifact:
+    '''This class is used to create the model trainer artifact.'''
+
+    trained_model_filepath:str 
+    metric_artifact:ClassificationMetricArtifact
+
+@dataclass
+class ModelEvaluationArtifact:
+    '''This class is used to create the model evaluation artifact.'''
+
+    is_model_accepted:bool
+    changed_accuracy:float
+    s3_model_path:str 
+    trained_model_path:str
+
+@dataclass
+class ModelPusherArtifact:
+    '''This class is used to create the model pusher artifact.'''
+
+    bucket_name:str
+    s3_model_path:str
--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -40,3 +40,54 @@ class DataValidationConfig:
 
     data_validation_dir = os.path.join(training_pipeline_config.artifact_dir, DATA_VALIDATION_DIR)
     validation_report_filepath = os.path.join(data_validation_dir, DATA_VALIDATION_REPORT_FILENAME)
+
+@dataclass
+class DataTransformationConfig:
+
+    '''This class is used to create the data transformation configuration.
+    It creates the data transformation directory, transformed data directory,
+    transformed object directory, and the training and testing file paths.'''
+
+    data_transformation_dir: str = os.path.join(training_pipeline_config.artifact_dir, DATA_TRANSFORMATION_DIR)
+    transformed_train_filepath: str = os.path.join(data_transformation_dir, DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+                                                    TRAIN_FILENAME.replace("csv", "npy"))
+    transformed_test_filepath: str = os.path.join(data_transformation_dir, DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+                                                    TEST_FILENAME.replace("csv", "npy"))
+    transformed_object_filepath: str = os.path.join(data_transformation_dir,
+                                                    DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
+                                                    PREPROCESSING_OBJECT_FILENAME)
+    
+@dataclass
+class ModelTrainerConfig:
+    model_trainer_dir = os.path.join(training_pipeline_config.artifact_dir, MODEL_TRAINER_DIR)
+    trained_model_filepath = os.path.join(model_trainer_dir, MODEL_TRAINER_TRAINED_MODEL_DIR, MODEL_FILENAME)
+    expected_accuracy = MODEL_TRAINER_EXPECTED_SCORE
+    model_config_filepath = MODEL_TRAINER_MODEL_CONFIG_FILEPATH
+    _n_estimators = MODEL_TRAINER_N_ESTIMATORS
+    _min_samples_split = MODEL_TRAINER_MIN_SAMPLES_SPLIT
+    _min_samples_leaf = MODEL_TRAINER_MIN_SAMPLES_LEAF
+    _max_depth = MIN_SAMPLES_SPLIT_MAX_DEPTH
+    _criterion = MIN_SAMPLES_SPLIT_CRITERION
+    _random_state = MIN_SAMPLES_SPLIT_RANDOM_STATE
+
+@dataclass
+class ModelEvaluationConfig:
+    '''This class is used to create the model evaluation configuration.'''
+
+    changed_threshold_score: float = MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE
+    bucket_name: str = MODEL_BUCKET_NAME
+    s3_model_key_path: str = MODEL_FILENAME
+
+@dataclass
+class ModelPusherConfig:
+    '''This class is used to create the model pusher configuration.'''
+
+    bucket_name: str = MODEL_BUCKET_NAME
+    s3_model_key_path: str = MODEL_FILENAME
+
+@dataclass
+class VehiclePredictorConfig:
+    '''This class is used to create the vehicle predictor configuration.'''
+
+    model_file_path: str = MODEL_FILENAME
+    model_bucket_name: str = MODEL_BUCKET_NAME
